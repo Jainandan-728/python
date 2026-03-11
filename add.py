97ea4a52e0fb29b20906793878f0fb4f93585f87
Jainandan-728 <nandan56562@gmail.com>
2026-03-11 10:42:01 +0530

    Update add.py

--- add.py
+++ add.py
@@ -4,7 +4,7 @@ def add_numbers(a, b):
     return a + b
 
 if __name__ == "__main__":
-    num1 = 6
-    num2 = 3
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
     result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is: {result}")
