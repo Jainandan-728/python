5557ed44769b90ccfa9668cf071975dd4b2251fa
Jainandan-728 <nandan56562@gmail.com>
2026-03-11 10:19:40 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,15 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+
+    print("10 + 5 =", calc.add(10, 5))
+    print("10 - 5 =", calc.subtract(10, 5))
