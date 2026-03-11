d2f97006189fd7d3e7d13dcd302a7276699ae3b6
Jainandan-728 <nandan56562@gmail.com>
2026-03-11 10:37:39 +0530

    Update and rename calculator.py to add.py

---
+++ add.py
@@ -0,0 +1,11 @@
+# add.py
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    # Example usage
+    num1 = 4
+    num2 = 6
+    result = add_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
